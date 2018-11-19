f3e47aad0e3cb65176a03b8922a434a1767e08ef
bachupr <bachupr@yahoo.com>
2018-11-19 15:12:35 -0500

    updated files as of 3 PM

---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+//Letter choices available
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Setting all to zero
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+//Computer choice
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+// Function to Update Guesses Left
+function updateGuessesLeft() {
+    // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+
+function updateLetterToGuess() {
+    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+
+function updateGuessesSoFar() {
+    // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+
+// Function to reset
+var reset = function() {
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
+
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+//User's' guess
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var check = computerChoices.includes(userGuess);
+
+    if (check === false) {
+        
+        alert("That was not a valid Character Selected, try again?");
+        return false;
+    } else if (check === true) {
+        //Check for the letter selected 
+        guessesLeft--;
+        guessedLetters.push(userGuess);
+        updateGuessesLeft();
+        updateGuessesSoFar();
+
+        if (guessesLeft > 0) {
+            if (userGuess == letterToGuess) {
+                wins++;
+               
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                userGuess = userGuess.toUpperCase();
+                alert("You are psychic and Won the Game!  " + userGuess);
+                reset();
+            }
+        } else if (guessesLeft == 0) {
+            // Update the html to display the loss 
+            losses++;
+            
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("You're not psychic, please try again?");
+             
+            reset();
+        }
+        return false;
+    } else {
+        alert("Error");
+    }
+
+};
